dd912c5e6c31e8d6b4ccf57b5133df338d7af583
salvaH21 <sholivares90@gmail.com>
2023-12-20 10:30:00 +0100

    bucle mover personas

--- orm.py
+++ orm.py
@@ -10,25 +10,43 @@ class Persona:
         self.posx = random.randint(0,1024)
         self.posy = random.randint(0,1024)
         self.radio = 30
+        self.direccion = 0
+        self.color = "blue"
+        self.entidad = ""
     def dibuja(self):
-        lienzo.create_oval(
+        self.entidad = lienzo.create_oval(
             self.posx-self.radio/2,
             self.posy-self.radio/2,
             self.posx+self.radio/2,
             self.posy+self.radio/2,
-            fill="red")
+            fill=self.color)
+    def mueve(self):
+        lienzo.move(self.entidad,5,0)
         
         
-
+#Creo una ventana
 raiz = tk.Tk()
 
+#En la ventana creo un lienzo
 lienzo = tk.Canvas(width=1024,height=1024)
 lienzo.pack()
 
+#En la colección introduzco instancias de personas
 for i in range(0,numeropersonas):
     personas.append(Persona())
-
+    
+#Dibujar cada persona de la colección
 for persona in personas:
-    persona.dibuja()
+    persona.dibuja
+    
+#Creo un buble repetitivo
+def bucle():
+    #Muevo a cada persona de la colección
+    for persona in personas:
+        persona.mueve()
+    raiz.after(1000,bucle)
+    
+#Ejecuto el bucle
+bucle()
 
 raiz.mainloop()
