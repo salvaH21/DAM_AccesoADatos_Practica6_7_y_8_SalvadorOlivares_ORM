eb5ed1ada0a2d48fb2fdb8e160ecc872ebe1253d
salvaH21 <sholivares90@gmail.com>
2023-12-20 10:11:43 +0100

    array de personas

--- orm.py
+++ orm.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 import random
 
+personas = []
+numeropersonas = 20
+
+
 class Persona:
     def __init__(self):
-        self.posx = random.randint(0,512)
-        self.posy = random.randint(0,512)
+        self.posx = random.randint(0,1024)
+        self.posy = random.randint(0,1024)
         self.radio = 30
     def dibuja(self):
         lienzo.create_oval(
@@ -21,7 +25,10 @@ raiz = tk.Tk()
 lienzo = tk.Canvas(width=1024,height=1024)
 lienzo.pack()
 
-persona = Persona()
-persona.dibuja()
+for i in range(0,numeropersonas):
+    personas.append(Persona())
+
+for persona in personas:
+    persona.dibuja()
 
 raiz.mainloop()
