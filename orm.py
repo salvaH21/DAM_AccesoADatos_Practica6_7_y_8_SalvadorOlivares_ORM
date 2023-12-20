ca3af5ea901105413bc14d9be4bad69cadf5f76b
salvaH21 <sholivares90@gmail.com>
2023-12-20 09:57:03 +0100

    crear persona

--- orm.py
+++ orm.py
@@ -1,8 +1,20 @@
 import tkinter as tk
 
+class Persona:
+    def __init__(self):
+        self.posx = 512
+        self.posy = 512
+    def dibuja(self):
+        lienzo.create_oval(30,30,60,60,fill="red")
+        
+        
+
 raiz = tk.Tk()
 
 lienzo = tk.Canvas(width=1024,height=1024)
 lienzo.pack()
+
+persona = Persona()
+persona.dibuja()
 
 raiz.mainloop()
