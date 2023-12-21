6c18ce73631ded6cad721dc1c29732cb2ee57141
salvaH21 <sholivares90@gmail.com>
2023-12-21 14:01:08 +0100

    colores al azar, velocidad al azar, radio al azar

--- orm.py
+++ orm.py
@@ -9,9 +9,10 @@ class Persona:
     def __init__(self):
         self.posx = random.randint(0,1024)
         self.posy = random.randint(0,1024)
-        self.radio = 30
+        self.radio = valor(30,60)
         self.direccion = random.randint(0,360)
-        self.color = "blue"
+        self.color = colores()
+        self.velocidad = valor(1,10)
         self.entidad = ""
     def dibuja(self):
         self.entidad = lienzo.create_oval(
@@ -22,16 +23,28 @@ class Persona:
             fill=self.color)
     def mueve(self):
         self.colisiona()
+        desplazamientox = self.velocidad * math.cos(self.direccion)
+        desplazamientoy = self.velocidad * math.sin(self.direccion)
         lienzo.move(
             self.entidad,
-            math.cos(self.direccion),
-            math.sin(self.direccion))
-        self.posx += math.cos(self.direccion)
-        self.posy += math.sin(self.direccion) 
+            desplazamientox,
+            desplazamientoy)
+        self.posx += desplazamientox
+        self.posy += desplazamientoy
+        #self.direccion += random.uniform(-1, 1)
     def colisiona(self):
         if self.posx < 0 or self.posx >1024 or self.posy < 0 or self.posy >1024:
             self.direccion += math.atan2(self.posy,self.posx)
 
+            
+def colores():
+    listacolores = ["red","blue","yellow","green","orange","purple","pink","black"]
+    colorazar = listacolores[random.randint(0,len(listacolores)-1)]
+    return colorazar
+
+def valor(numero1,numero2):
+    numero = random.randint(numero1,numero2)
+    return numero
 
 def guardarPersonas():
     print("guardo a los jugadores")
@@ -67,7 +80,7 @@ except:
 #En la colección introduzco instancias de personas en el caso de que no existan
 print(len(personas))
 if len(personas) == 0:
-    numeropersonas = 4
+    numeropersonas = 8
     for i in range(0,numeropersonas):
         personas.append(Persona())
     
@@ -80,7 +93,7 @@ def bucle():
     #Muevo a cada persona de la colección
     for persona in personas:
         persona.mueve()
-    raiz.after(1,bucle)
+    raiz.after(10,bucle)
     
 #Ejecuto el bucle
 bucle()
