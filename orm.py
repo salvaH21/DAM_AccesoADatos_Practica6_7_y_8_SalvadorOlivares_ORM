7698dad2c739a3b16dfa653607838146ab8e4e4c
salvaH21 <sholivares90@gmail.com>
2024-01-17 09:02:00 +0100

    guardar inventario en sql

--- orm.py
+++ orm.py
@@ -177,6 +177,18 @@ def guardarPersonas():
                 "'''+str(persona.inventario)+'''"
             )
             ''')
+        for recogible in persona.inventario:
+            peticion = '''
+            INSERT INTO recogibles
+            VALUES (
+                NULL,
+                '''+str(persona.entidad)+''',
+                "'''+str(recogible.posx)+'''",
+                "'''+str(recogible.posy)+'''",
+                "'''+str(recogible.color)+'''"
+            )
+            '''
+            cursor.execute(peticion)
     conexion.commit()
     conexion.close()
 
