879d777f43e2fa315e7edd7eb49914586613597a
salvaH21 <sholivares90@gmail.com>
2024-01-18 13:19:09 +0100

    VERSION CON INVENTARIO

--- orm.py
+++ orm.py
@@ -257,7 +257,7 @@ boton.pack()
 #En la colección introduzco instancias de personas en el caso de que no existan
 print("Personas encontradas: " + str(len(personas)))
 if not personas:
-    numeropersonas = 30
+    numeropersonas = 1
     for i in range(0,numeropersonas):
         personas.append(Persona())
     
